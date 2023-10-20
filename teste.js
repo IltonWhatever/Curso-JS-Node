d49adfe8aa6fe9b206dc84a87097b6fc3c7fb468
Ilton Whatever <ashpalete18@gmail.com>
2023-10-20 13:30:55 -0300

    Primeiro Server

--- teste.js
+++ teste.js
@@ -1,8 +1,11 @@
+// Request da biblioteca que é usada para criar o server
 var http = require('http');
 
+// Criando o serve e armazenando na variavel server
 var server = http.createServer( function(request, response){
-    request.end("<html><body> Teste </body></html>");
+    response.end("<html><body> Teste </body></html>");
     
 });
 
+// Usando a propriedade listen para "ouvir" na porta 3000
 server.listen(3000);
