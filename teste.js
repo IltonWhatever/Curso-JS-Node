2ed0d155ec8955d10cb44bdf39ff373cbd5bf333
Ilton Whatever <ashpalete18@gmail.com>
2023-10-20 13:42:12 -0300

    Habilitando rotas

--- teste.js
+++ teste.js
@@ -3,7 +3,20 @@ var http = require('http');
 
 // Criando o serve e armazenando na variavel server
 var server = http.createServer( function(request, response){
-    response.end("<html><body> Teste </body></html>");
+    // Armazenando a url que sera usada para identificar qual rota seguir
+    var rota = request.url;
+
+    // Identificando qual rota seguir com base na variavel
+    if (rota == "/home"){
+        response.end("<html><body> <h1> Pagina Home </h1> </body></html>");
+    }else if(rota == "/teste"){
+        response.end("<html><body> <h1> Pagina Teste </h1> </body></html>");
+    }else{
+        response.end("<html><body> <h1> Pagina Inicial </h1> </body></html>");
+    };
+
+
+    
     
 });
 
