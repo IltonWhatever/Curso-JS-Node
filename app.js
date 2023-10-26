3f8f82731f9bee904080a7b5f16bfbcfb12efa53
Ilton Whatever <ashpalete18@gmail.com>
2023-10-26 12:32:11 -0300

    Instalando o Consign e rotas automaticas

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 app = require('./config/server');
 
-// Importando o modulo da rota Noticias.
-var rotaNoticias = require('./app/routes/noticias')(app);
-var rotaHome = require('./app/routes/home')(app);
-var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
-
 // Usando a propriedade listen para ouvir na porta desejada
 app.listen(3000, function(){
     console.log('Servidor rodando com Express na porta 3000')
--- config/server.js
+++ config/server.js
@@ -1,10 +1,11 @@
-// Configs da aplicação
 var express = require('express');
+var consign = require('consign');
+
 var app = express();
 
-// Config para setar o uso da rederização pelo EJS para o express
-app.set('view engine', 'ejs');
-// Apotando o diretorio padrão das views
-app.set('views', './app/views')
+app.set('view engine', 'ejs'); // Config para setar o uso da rederização pelo EJS para o express
+app.set('views', './app/views'); // Apotando o diretorio padrão das views
+
+consign().include('app/routes').into(app); // Reconhece as rotas modulos da pasta rotas e envia para APP automatizando a importação de rotas
 
 module.exports = app;
