9ef085c6d9fb1c2da9bb5b57f4771e0fbe6654e8
Ilton Whatever <ashpalete18@gmail.com>
2023-11-09 13:27:57 -0300

    Criando a rota que irar receber o formulario POST

---
+++ app/routes/admin.js
@@ -0,0 +1,13 @@
+module.exports = function(app){
+    
+    // Rota Inclusão de noticias formulario.
+    app.get('/formulario_inclusao_noticia', function(request, response){
+        response.render('admin/form_add_noticia')
+    });
+
+    // Rota POST que recebe o formulario.
+    app.post('/noticias/salvar', function(request, response){
+        let noticias = request.body;
+        response.send('Pagina Teste');
+    });
+}
