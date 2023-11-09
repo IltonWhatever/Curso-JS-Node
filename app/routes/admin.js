619590a1c38b1e239469a498e1464b4175eea736
Ilton Whatever <ashpalete18@gmail.com>
2023-11-09 13:58:25 -0300

    Inserindo os dados do formulario e retornando list

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,13 +1,24 @@
-module.exports = function(app){
+module.exports = function(application){
     
     // Rota Inclusão de noticias formulario.
-    app.get('/formulario_inclusao_noticia', function(request, response){
+    application.get('/formulario_inclusao_noticia', function(request, response){
         response.render('admin/form_add_noticia')
     });
 
     // Rota POST que recebe o formulario.
-    app.post('/noticias/salvar', function(request, response){
-        let noticias = request.body;
-        response.send(noticias);
+    application.post('/noticias/salvar', function(request, response){
+        let noticia = request.body; // Variavel contendo o JSON da noticia feita no formulario.
+
+        let connection = application.config.dbConnection(); // Referencia que recupera a conexão com o BD
+        let noticiasModel = application.app.models.noticiasModel; // Recuperando o modulo Get e instancioando na variavel
+
+        // Com a variavel instanciada, recuperamos o modulo getNoticia
+        noticiasModel.salvarNoticia(noticia, connection, function(error, result){
+            if(error){
+                console.log(error);
+            }else {
+                response.redirect('/noticias');
+            }
+        });
     });
 }
