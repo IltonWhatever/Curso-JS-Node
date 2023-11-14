22b8be3c1cd4d3c27b0fe1e66a12e1a6de1d5991
Ilton Whatever <ashpalete18@gmail.com>
2023-11-14 14:41:11 -0300

    Validando os dados que viram de formularios

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -9,6 +9,22 @@ module.exports = function(application){
     application.post('/noticias/salvar', function(request, response){
         let noticia = request.body; // Variavel contendo o JSON da noticia feita no formulario.
 
+        // Express-Validator - Os codigos a seguir servem para validar os dados dos formularios.
+        request.assert('titulo','Titulo é obrigatorio').notEmpty();
+        request.assert('resumo','Resumo é obrigatorio').notEmpty();
+        request.assert('resumo','Resumo deve conter ao minimo 10 caracteres e no maximo 100').len(10, 100);
+        request.assert('autor','Autor é obrigatorio').notEmpty();
+        request.assert('data_noticia','Autor é obrigatorio').notEmpty().isDate({format: 'YYYY-MM-DD'});
+        request.assert('noticia','Noticia é obrigatorio').notEmpty();
+        // Verificando se houve algum erro
+        const erros = request.validationErrors();
+        
+        if(erros){
+            response.render('admin/form_add_noticia');
+            return;
+        }
+
+
         let connection = application.config.dbConnection(); // Referencia que recupera a conexão com o BD
         let noticiasModel = new application.app.models.NoticiasDAO(connection); // Recuperando o modulo Get e instancioando na variavel
 
