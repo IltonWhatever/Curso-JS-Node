9487598a5e0c0512140f08e0bb1202288b05b161
Ilton Whatever <ashpalete18@gmail.com>
2023-11-21 12:31:56 -0300

    Incluindo recursos estaticos

--- app/controllers/noticias.js
+++ app/controllers/noticias.js
@@ -1,8 +1,8 @@
 // Modulos de controles para rotas referentes a noticias
 
 module.exports.noticias = function(application, request, response){
-    let connection = application.config.dbConnection(); // Referencia que recupera a conexão com o BD
-    let noticiasModel = new application.app.models.NoticiasDAO(connection); // Acessando modulos que foram carregados em APP pelo Consign
+    const connection = application.config.dbConnection(); // Referencia que recupera a conexão com o BD
+    const noticiasModel = new application.app.models.NoticiasDAO(connection); // Acessando modulos que foram carregados em APP pelo Consign
 
     noticiasModel.getNoticias(function(error, result){
         if(error){
@@ -14,8 +14,8 @@ module.exports.noticias = function(application, request, response){
 }
 
 module.exports.noticia = function(application, request, response){
-    let connection = application.config.dbConnection();
-    let noticiasModel = new application.app.models.NoticiasDAO(connection);
+    const connection = application.config.dbConnection();
+    const noticiasModel = new application.app.models.NoticiasDAO(connection);
 
     noticiasModel.getNoticia(function(error, result){
         if(error){
